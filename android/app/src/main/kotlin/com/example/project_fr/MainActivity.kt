7047f1e09582a937a5d1b739c77243de6b6d219d
Rohan <rohansakhare130@gmail.com>
2024-10-16 22:02:34 +0530

    feat: main.dart

---
+++ android/app/src/main/kotlin/com/example/project_fr/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_fr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.deepPurple
+      ),
+      // home:Container_Sized(),
+    );
+  }
+}
